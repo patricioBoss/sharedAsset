9e31d0b8d38feb60420915ccd97d661eb84deadc
Mbata Precious <patriciomaco414@gmail.com>
2023-03-13 16:24:56 +0100

    made changes to free space

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,24 @@
-import { m } from 'framer-motion';
+import { m } from "framer-motion";
 // next
-import NextLink from 'next/link';
+import NextLink from "next/link";
 // @mui
-import { styled } from '@mui/material/styles';
-import { Box, Button, Typography, Container } from '@mui/material';
+import { styled } from "@mui/material/styles";
+import { Box, Button, Typography, Container } from "@mui/material";
 // layouts
-import Layout from '../layouts';
+import Layout from "../layouts";
 // components
-import Page from '../components/Page';
-import { MotionContainer, varBounce } from '../components/animate';
+import Page from "../components/Page";
+import { MotionContainer, varBounce } from "../components/animate";
 // assets
-import { PageNotFoundIllustration } from '../assets';
+import { PageNotFoundIllustration } from "../assets";
+import Image from "next/image";
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled('div')(({ theme }) => ({
-  display: 'flex',
-  height: '100%',
-  alignItems: 'center',
+const RootStyle = styled("div")(({ theme }) => ({
+  display: "flex",
+  height: "100%",
+  alignItems: "center",
   paddingTop: theme.spacing(15),
   paddingBottom: theme.spacing(10),
 }));
@@ -33,29 +34,20 @@ Page404.getLayout = function getLayout(page) {
 export default function Page404() {
   return (
     <Page title="404 Page Not Found" sx={{ height: 1 }}>
-      <RootStyle>
-        <Container component={MotionContainer}>
-          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
-            <m.div variants={varBounce().in}>
-              <Typography variant="h3" paragraph>
-                Sorry, page not found!
-              </Typography>
-            </m.div>
-            <Typography sx={{ color: 'text.secondary' }}>
-              Sorry, we couldn’t find the page you’re looking for. Perhaps you’ve mistyped the URL? Be sure to check
-              your spelling.
-            </Typography>
-            <m.div variants={varBounce().in}>
-              <PageNotFoundIllustration sx={{ height: 260, my: { xs: 5, sm: 10 } }} />
-            </m.div>
-            <NextLink href="/">
-              <Button size="large" variant="contained">
-                Go to Home
-              </Button>
-            </NextLink>
-          </Box>
-        </Container>
-      </RootStyle>
+      <div>
+        <div className=" h-screen flex flex-col  space-y-20 pt-10 items-center justify-center text-center">
+          <p className="text-2xl">
+            ...Sorry resource could not be found, <br />
+            Please try again
+          </p>
+          <Image
+            src={"/img/404-landscape.svg"}
+            alt="unknown"
+            width={395}
+            height={395}
+          />
+        </div>
+      </div>
     </Page>
   );
 }
